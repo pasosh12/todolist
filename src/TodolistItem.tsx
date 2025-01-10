8046d4881ee2ff7a76ac6d65564d990d210a19e8
Daniil <daniil.shishaev@gmail.com>
2025-01-10 23:31:39 +0300

    adding and deleting tasks integration

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,14 +1,19 @@
 import {FilterValuesType, TaskType} from "./App.tsx";
 import {Button} from "./Button.tsx";
+import {useRef,
+    useState
+} from "react";
 
 type TodoListItemPropsType = {
     title: string,
     subTitle: string,
     description: string,
     tasks: TaskType[],
-    deleteTask: (id: number) => void,
+    deleteTask: (id: string) => void,
+    addTask: (title: string) => void,
     changeToDoListFilter: (filter: FilterValuesType) => void
 }
+const [taskTitle, setTaskTitle] = useState<string>("")
 
 export const TodolistItem = ({
                                  title,
@@ -16,18 +21,38 @@ export const TodolistItem = ({
                                  description,
                                  tasks,
                                  deleteTask,
-                                 changeToDoListFilter
+                                 changeToDoListFilter,
+                                 // addTask
                              }: TodoListItemPropsType) => {
 
-    const createOnClickHandler=(filter:FilterValuesType) => ()=>(changeToDoListFilter(filter))
+    const createOnClickHandler = (filter: FilterValuesType) => () => (changeToDoListFilter(filter))
+    const inputRef = useRef<HTMLInputElement>(null)
+
+
     return (
         <div>
             <h3>{title}</h3>
             <h4>{subTitle}</h4>
             <p>{description}</p>
             <div>
-                <input/>
-                <Button title={'+'}/>
+                <input ref={inputRef}
+                       value={taskTitle}
+                       onChange={(e) =>{
+                           debugger;
+                    setTaskTitle(e.currentTarget.value)}
+                    }
+                />
+                <Button title={'+'}
+                //     //     onClick={() => {
+                //     // if (inputRef.current) {
+                //     //     // debugger;
+                //     //     // console.log(inputRef.current)
+                //     //     addTask(taskTitle);
+                //     //     inputRef.current.value='';
+                //     // }
+                // {/*}*/}
+                // {/*    }*/}
+                />
             </div>
             {
                 tasks.length === 0 ? (
@@ -35,11 +60,14 @@ export const TodolistItem = ({
                 ) : (
                     <ul>
                         {tasks.map((item: TaskType) => {
+                            const deleteTaskHandler=()=>{
+                                deleteTask(item.id)
+                            }
                             return (
                                 <li key={item.id}>
                                     <input type="checkbox" checked={item.isDone}/>
                                     <span>{item.title}</span>
-                                    <Button title={'x'} onClickHandler={() => deleteTask(item.id)}/>
+                                    <Button title={'x'} onClick={deleteTaskHandler}/>
                                 </li>
                             )
                         })}
@@ -50,7 +78,7 @@ export const TodolistItem = ({
             <div>
                 <Button title={'All'} onClickHandler={createOnClickHandler('all')}/>
                 <Button title={'Active'} onClickHandler={createOnClickHandler('active')}/>
-                <Button title={'Completed'} onClickHandler={ createOnClickHandler('completed')}/>
+                <Button title={'Completed'} onClickHandler={createOnClickHandler('completed')}/>
 
             </div>
         </div>
