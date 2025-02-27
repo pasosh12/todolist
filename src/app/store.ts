cc734e3b432b855a19d0f641fbbe2cebffebd221
Daniil <daniil.shishaev@gmail.com>
2025-02-27 15:43:14 +0300

    redux store created

---
+++ src/app/store.ts
@@ -0,0 +1,19 @@
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
+import {tasksReducer} from "../model/tasks-reducer.ts";
+import {todolistsReducer} from "../model/todolists-reducer.ts";
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+export const store = configureStore({
+    reducer: rootReducer,
+})
+
+// автоматическое определение типа всего объекта состояния
+export type RootState = ReturnType<typeof store.getState>
+// автоматическое определение типа метода dispatch
+export type AppDispatch = typeof store.dispatch
+//@ts-ignore
+window.store = store;
