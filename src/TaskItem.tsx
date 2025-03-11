c971e13d624a2f9ce7720b01128d2f2a6562ea7d
Daniil <daniil.shishaev@gmail.com>
2025-03-11 18:32:30 +0300

    separate Tasks components created

---
+++ src/TaskItem.tsx
@@ -0,0 +1,49 @@
+import {ChangeEvent} from 'react';
+import {changeTaskStatusAC, changeTaskTitleAC, deleteTaskAC} from "./model/tasks-reducer.ts";
+import {Checkbox, IconButton, ListItem} from "@mui/material";
+import {getListItemsSx} from "./TodolistItem.styles.ts";
+import {EditableSpan} from "./EditableSpan.tsx";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {useAppDispatch} from "./common/hooks/useAppDispatch.ts";
+import {TaskType} from "./App.tsx";
+
+type Props = {
+    task: TaskType
+    todolistId: string
+}
+export const TaskItem = (props: Props) => {
+    const id = props.todolistId
+    const task = props.task
+    const dispatch = useAppDispatch();
+    const deleteTask = () => {
+        dispatch(deleteTaskAC({todolistId: id, taskId: task.id}))
+    }
+    const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
+        const newStatusValue = e.currentTarget.checked
+        dispatch(changeTaskStatusAC({
+            todolistId: props.todolistId,
+            taskId: task.id,
+            newStatus: newStatusValue
+        }))
+    }
+    const changeTaskTitle = (newTitle: string) => {
+        dispatch(changeTaskTitleAC({todolistId: id, taskId: task.id, newTitle}))
+    }
+
+    return (
+        <ListItem sx={getListItemsSx(task.isDone)}
+                  disablePadding={true}
+                  key={task.id}>
+            <Checkbox size={'medium'}
+                      onChange={changeTaskStatus}
+                      checked={task.isDone}/>
+            <EditableSpan title={task.title}
+                          onChangeTitle={changeTaskTitle}/>
+            <IconButton onClick={deleteTask}>
+                <DeleteIcon fontSize="small"/>
+            </IconButton>
+        </ListItem>
+    )
+}
+
+
---
+++ src/Tasks.tsx
@@ -0,0 +1,39 @@
+import {List} from "@mui/material";
+import {TodolistType} from "./App.tsx";
+import {useAppSelector} from "./common/hooks/useAppSelector.ts";
+import {selectTasks} from "./model/tasks-selectors.ts";
+import {TaskItem} from "./TaskItem.tsx";
+
+type Props = {
+    todolist: TodolistType
+}
+export const Tasks = ({todolist}: Props) => {
+    const {id, filter} = todolist
+
+    const tasks = useAppSelector(selectTasks);
+
+    const todolistTasks = tasks[id];
+    let filteredTasks = todolistTasks
+    if (filter === "active") {
+        filteredTasks = todolistTasks.filter(t => !t.isDone);
+    }
+    if (filter === "completed") {
+        filteredTasks = todolistTasks.filter(t => t.isDone);
+    }
+    return (
+        <>
+            {filteredTasks.length === 0 ? (
+                    <p>Empty List</p>
+                ) :
+                <List>
+                    {filteredTasks.map(task => {
+                            return (
+                                <TaskItem task={task} todolistId={id} key={task.id}/>
+                            )
+                        })
+                    }
+                </List>
+            }
+        </>
+    );
+};
