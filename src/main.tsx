6b9c6f0cea3cf9bc9b8a3a4a698babb0dd850989
Daniil <daniil.shishaev@gmail.com>
2025-04-01 18:57:04 +0300

    api data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {App} from './app/App.tsx'
+// import {App} from './app/App.tsx'
 import {store} from "./app/store.ts";
 import {Provider} from "react-redux";
+import {AppHttpRequests} from "@/app/AppHttpRequests.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <App/>
+        <AppHttpRequests/>
+        {/*<App/>*/}
     </Provider>
 )
