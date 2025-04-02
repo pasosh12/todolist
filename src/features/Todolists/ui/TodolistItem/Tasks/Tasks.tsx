2d656c0adefe0148710776829c644937471b18bd
Daniil <daniil.shishaev@gmail.com>
2025-04-02 15:23:35 +0300

    filtering bug fixed

--- src/features/Todolists/ui/TodolistItem/Tasks/Tasks.tsx
+++ src/features/Todolists/ui/TodolistItem/Tasks/Tasks.tsx
@@ -1,7 +1,7 @@
 import {List} from "@mui/material";
 import {TodolistType} from "@/app/App.tsx";
 import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
-import {selectTasks} from "../../../model/tasks-selectors.ts";
+import {selectTasks} from "../../../model/tasks-reducer.ts";
 import {TaskItem} from "./TaskItem/TaskItem.tsx";
 
 type Props = {
@@ -9,10 +9,10 @@ type Props = {
 }
 export const Tasks = ({todolist}: Props) => {
     const {id, filter} = todolist
-
+    // debugger
     const tasks = useAppSelector(selectTasks);
 
-    const todolistTasks = tasks[id];
+    const todolistTasks = tasks[id] || [];
     let filteredTasks = todolistTasks
     if (filter === "active") {
         filteredTasks = todolistTasks.filter(t => !t.isDone);
@@ -27,10 +27,10 @@ export const Tasks = ({todolist}: Props) => {
                 ) :
                 <List>
                     {filteredTasks?.map(task => {
-                            return (
-                                <TaskItem task={task} todolistId={id} key={task.id}/>
-                            )
-                        })
+                        return (
+                            <TaskItem task={task} todolistId={id} key={task.id}/>
+                        )
+                    })
                     }
                 </List>
             }
