0b0c97586ffe584197b0d8ae51a57691d938f10a
Daniil <daniil.shishaev@gmail.com>
2025-04-09 13:19:53 +0300

    createAppSlice.ts for async thunks reducers

--- src/features/Todolists/model/tasks-reducer.ts
+++ src/features/Todolists/model/tasks-reducer.ts
@@ -4,55 +4,6 @@ import { createTodolistAC, deleteTodolistAC } from "@/features/Todolists/model/t
 
 const initialState: TasksState = {}
 
-// export const changeTaskTitleAC = createAction<{
-//     todolistId: string,
-//     taskId: string,
-//     newTitle: string
-// }>("tasks/changeTaskTitle");
-// export const changeTaskStatusAC = createAction<{
-//     todolistId: string,
-//     taskId: string,
-//     newStatus: boolean
-// }>("tasks/changeTaskStatus");
-// export const deleteTaskAC = createAction<{
-//     todolistId: string, taskId: string
-// }>('tasks/deleteTask')
-// export const createTaskAC = createAction('tasks/createTask', (title: string, todolistId: string) => {
-//     return {payload: {id: nanoid(), title, todolistId, isDone: false}}
-// })
-
-// export const tasksReducer = createReducer(initialState, builder => {
-//     builder
-//         .addCase(deleteTodolistAC, (state, action) => {
-//             delete state[action.payload.todolistId]
-//         }).addCase(createTodolistAC, (state, action) => {
-//         state[action.payload.id] = []
-//     })
-//         .addCase(changeTaskTitleAC, (state, action) => {
-//             const task = state[action.payload.todolistId].find(task =>
-//                 task.id === action.payload.taskId)
-//             if (task) {
-//                 task.title = action.payload.newTitle
-//             }
-//         })
-//         .addCase(changeTaskStatusAC, (state, action) => {
-//             const task = state[action.payload.todolistId].find(task =>
-//                 task.id === action.payload.taskId)
-//             if (task) {
-//                 task.isDone = action.payload.newStatus
-//             }
-//
-//         })
-//         .addCase(createTaskAC, (state, action) => {
-//             state[action.payload.todolistId].unshift(action.payload)
-//         })
-//         .addCase(deleteTaskAC, (state, action) => {
-//             const taskIndex = state[action.payload.todolistId].findIndex(task => task.id === action.payload.taskId)
-//             if (taskIndex !== -1) {
-//                 state[action.payload.todolistId].splice(taskIndex, 1);
-//             }
-//         })
-// })
 export const tasksSlice = createSlice({
   name: "tasks",
   initialState: initialState,
